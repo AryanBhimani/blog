da0f2e7fed925e7f4fbb853ffbdaee9092357dcb
AryanBhimani <aryan.bhimani.93@gmail.com>
2025-12-30 10:57:31 +0530

    Mobintix

--- js/footer.js
+++ js/footer.js
@@ -3,4 +3,8 @@ fetch("/components/footer.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("footer-placeholder").innerHTML = data;
+    const yearSpan = document.getElementById('current-year');
+    if (yearSpan) {
+      yearSpan.textContent = new Date().getFullYear();
+    }
   });
