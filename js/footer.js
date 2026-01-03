d3f8ba2eeb5bf5ef439f78fb249d9590908803c1
Aryan Bhimani <aryan.bhimani.93@gmail.com>
2026-01-03 15:09:22 +0530

    Mobintix

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,6 @@
 // js/footer.js
+import { supabase } from "./supabase/supabaseClient.js";
+
 fetch("./components/footer.html")
   .then(res => res.text())
   .then(data => {
@@ -7,4 +9,30 @@ fetch("./components/footer.html")
     if (yearSpan) {
       yearSpan.textContent = new Date().getFullYear();
     }
+
+    // Update footer based on authentication state
+    updateFooter();
+
+    // Listen for auth state changes
+    supabase.auth.onAuthStateChange(() => {
+      updateFooter();
+    });
   });
+
+async function updateFooter() {
+  const { data } = await supabase.auth.getUser();
+  const user = data?.user;
+
+  // Get the "My Profile" link from footer
+  const profileLink = document.querySelector('.footer-column a[href="./profile.html"]');
+  
+  if (profileLink) {
+    if (user) {
+      // User is logged in: Show "My Profile" link
+      profileLink.parentElement.style.display = "";
+    } else {
+      // User is not logged in: Hide "My Profile" link
+      profileLink.parentElement.style.display = "none";
+    }
+  }
+}
